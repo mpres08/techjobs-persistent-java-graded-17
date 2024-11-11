424aa036067f4a8a94d4db201fcee48adfdc1f75
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-11-11 14:10:36 -0600

    failing sql test in task 2

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotNull
+    @Size(min = 3, max = 20)
+    private String location;
+
+    public Employer() {
+    }
+
+    public @NotNull @Size(min = 3, max = 20) String getLocation() {
+        return location;
+    }
+
+    public void setLocation(@NotNull @Size(min = 3, max = 20) String location) {
+        this.location = location;
+    }
 }
